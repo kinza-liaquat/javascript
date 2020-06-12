2c43e942b97c4ff97275f26e1229dd1fd6f1a9c6
kinza liaquat <kinzag756@gmail.com>
2020-06-12 18:20:44 +0500

    add javascript file

---
+++ index.js
@@ -0,0 +1,364 @@
+// browser mein space k lye &nbsp; use hta
+// browser mein next line m jne k lye <br> use hta
+// alert mein space k lye
+// alert mein next line k lye \n use hta
+
+
+// assignmnt 1 alert message
+// alert("JavaScript Alerts\n\nError! Please enter a valid password.");
+// alert("Welcome to JS Land... \nHappy coding!" );
+
+// assignmnt 2 alert msg
+// var username = "Mark";
+// username = "john deo";
+// alert(username);
+
+// show msg
+// var message = "Pizza\nPizz\nPiz\nPi\nP";
+// alert(message);
+
+// show age
+// var message = "15 year old";
+// alert (message);
+
+// show msg
+// var show = "-------@@@@@&&&&&&-------";
+// alert(show);
+
+// show age
+// var age;
+// age = "I am 23 years old";
+// alert(age);
+
+// show birthyear and datatype
+// var birthyear;
+// document.write("My birth year is 1996 <br/> Data type of my declared variable is Number");
+
+// show shopping msg
+// var name = "John";
+// var product = "t-shirts";
+// var quantity = 5;
+// document.write("John ordered 5 T-shirt(s) on XYZ Clothing store.");
+
+// declare 3 var in same line
+// var name , age , email;
+// alert(name);
+// alert(age);
+// alert(email);
+
+// show sum of num
+// var num = 3;
+// var anotherNum = 5;
+// var popularNumber = num + anotherNum;
+
+// show initilize var
+// var num;
+// num = 5;
+// document.write("initilize variable is :5");
+
+// alert sum Number
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+// alert(popularNumber);
+
+// show marks sheet on browser
+// document.write("Marks Sheet<br/><br/><br/><br/>");
+// var totalmarks = 980;
+// var obtainmarks = 804;
+// var percentage = (obtainmarks/totalmarks*100);
+// document.write("Total Marks: " + totalmarks + "<br>");
+// document.write("Obtain Marks: " + obtainmarks + "<br>");
+// document.write("percentage: " + percentage + "<br>");
+
+// convert currency
+// document.write("Currency in PKR<br><br><br>");
+// var usdoller = 10;
+// var riyal = 25;
+// var convert = (10*104.80)+(25*28);
+// document.write("Total currency in PKR : "+ convert);
+
+// math expression
+// var n = 5;
+// var addNewnum = 5;
+// var result = (n+addNewnum)*10/2;
+// document.write("Result: " + result);
+
+// age calculate
+// var currentyear = 2020;
+// var birthyear = 1996;
+// var calculate = (currentyear-birthyear);
+// document.write("Current Year: " + currentyear + "<br>");
+// document.write("Birth Year: " + birthyear + "<br>");
+// document.write("Your Age is : " + calculate + "<br>");
+
+// area of circle
+// var radius = 20;
+// var circumference = 2*3.142*20;
+// var area = 3.142*20*20;
+// document.write("Radius of a circle : " + radius + "<br>");
+// document.write("The circumference is : " + circumference + "<br>");
+// document.write("The Area is : " + area + "<br>");
+
+// chat 6-9
+// increment + decreemnt
+// var a = 10;
+// var preincre = ++a;
+// var postincre = a++;
+// var predec = --a;
+// var postdec = a--;
+// document.write("Result: " + " <br>");
+// document.write("The value of a is: " + a + "<br/>");
+// document.write("..............................."+ "<br> <br><br>");
+// document.write("The value of ++a is: " + preincre + "<br>");
+// document.write("Now the value of a is : 11" + "<br><br><br>");
+// document.write("The value of ++a is: " + postincre + "<br>");
+// document.write("Now the value of a is : 12" + "<br><br><br>");
+// document.write("The value of ++a is: " + predec + "<br>");
+// document.write("Now the value of a is : 11" + "<br><br><br>");
+// document.write("The value of ++a is: " + postdec + "<br>");
+// document.write("Now the value of a is : 10" + "<br><br><br>");
+
+// show the output
+// var a = 2;
+//  var b = 1;
+// var deca = --a ;
+// document.write(" a is: " + a + "<br>");
+// var decb = --b;
+// document.write(" b is: " + b + "<br>");
+// var sub = --a - --b;
+// var increb = ++b;
+// var add = --a - --b + ++b;
+// var postdecb = b--;
+// var result = (--a) - (--b) + (++b) + (b--);
+// document.write(" result is: " + result + "<br>");
+
+// use prompt
+// var username = prompt ("Whats's your name?");
+// document.write(username);
+
+// subject result card table (Error)
+// var first_subject = prompt("Enter first subject");
+// var second_subject = prompt("Enter second subject");
+// var third_subject = prompt("Enter third subject");
+// var totalMarks = 100;
+// var firstObtainmark = prompt("Enter first subject obtain marks");
+// var secondObtainmark = prompt("Enter second subject obtain marks");
+// var thirdObtainmark = prompt("Enter third subject obtain markst");
+// var firstPercentage = (firstObtainmarks/TotalMarks*100);
+// var seondPercentage = (secondObtainmarks/TotalMarks*100);
+// var thirdPercentage = (thirdObtainmarks/TotalMarks*100);
+// var grandTotal = totalMarks + totalMarks + totalMarks;
+// var grandObtainmarks = firstObtainmark + secondObtainmark + thirdObtainmark;
+// var grandPercentage = firstPercentage + seondPercentage + thirdPercentage;
+// document.write("Subjects"&nbsp;"Total Marks"&nbsp;"Obtain Marks"&nbsp;"Percentage" + "<br/>");
+//
+// document.write(first_subject&nbsp; &nbsp;+ totalMarks&nbsp; &nbsp;&nbsp; + firstObtainmark &nbsp; &nbsp;&nbsp;&nbsp; + firstPercentage&nbsp; &nbsp; +"<br/>" );
+//
+// document.write(second_subject&nbsp; &nbsp;+ totalMarks&nbsp; &nbsp;&nbsp; + secondObtainmark &nbsp; &nbsp;&nbsp;&nbsp; + secondPercentage&nbsp; &nbsp; +"<br/>");
+//
+// document.write(third_subject&nbsp; &nbsp;+ totalMarks&nbsp; &nbsp;&nbsp; + thirdObtainmark &nbsp; &nbsp;&nbsp;&nbsp; + thirdPercentage + &nbsp; &nbsp; +"<br/>");
+// document.write(grandTotal&nbsp;&nbsp; + grandObtainmarks&nbsp;&nbsp; + grandPercentage );
+
+// chap 9-11
+// if use in two methods
+
+// var x = prompt("Enter your city");
+// if (x === "karachi") {
+  // alert("Welcome to city of lights")
+  // document.write("Welcome to city of lights")
+// }
+// if statement
+// var x = prompt("Enter your Gender");
+// if (x === "male") {
+//   alert("Good Morning Sir")
+// }
+// if (x === "female") {
+//   alert("Good Morning Ma'am")  }
+
+// if condition
+// var x = prompt ("Enter remaining fuel in liters");
+// if (x < 0.25 ) {
+//   alert("Please refill the fuel in your car");
+// } else {
+//   alert("No fuel required");
+// }
+
+// check whether
+// var a = 4;
+//  if (++a === 5) {
+//   alert("given condition for variable a is true");
+// }
+
+// secret Number
+// var secretNum = 10;
+// var x = prompt ("Guess the secret number");
+// if ( x === 10 ) {
+//   document.write("Bingo! Correct answer");
+// }
+// if ( x > 10) {
+//     document.write("Close enough to the correct answer");
+// }
+
+// divisible number
+// var x = prompt("Enter the number");
+// if ( x / 3 ) {
+//   document.write("divisible by 3")
+// } else {
+//   document.write("Not divisible by 3")
+// }
+
+// even or odd num
+// var x = prompt("Enter the number");
+// if ( x % 2 == 0 ) {
+//   document.write("Even Numbers")
+// } else {
+//   document.write("Odd Numbers")
+// }
+
+// tempereture
+// var t = prompt("Enter the tempereture");
+// if ( t > 40) {
+// document.write("It is too hot outside.")
+// } else if ( t > 30) {
+// document.write("The Weather today is Normal.")
+// } else if ( t > 20) {
+//   document.write("Today’s Weather is cool.")
+// } else if ( t > 10) {
+//    document.write("OMG! Today’s weather is so Cool.")
+// } else {
+//   document.write("Invalid Tempereture");
+// }
+
+// create calculator (error)
+// var first_num = prompt("Enter the first number");
+// var second_num = prompt("Enter the second number");
+// var op_add = first_num + second_num;            (error)
+// var op_sub = first_num - second_num;
+// var op_multi = first_num * second_num;
+// var op_div = first_num / second_num;
+// var op_mod = first_num % second_num;
+// document.write("Addition of two num: " + op_add + "<br>");
+// document.write("Subtraction of two num: " + op_sub + "<br>");
+// document.write("Multiplication of two num: " + op_multi + "<br>");
+// document.write("Division of two num: " + op_div + "<br>");
+// document.write("Mod of two num: " + op_mod + "<br>");
+
+// marks sheets (error)
+// var first_sub = prompt("Enter first subject marks");
+// var second_sub = prompt("Enter second subject marks");
+// var third_sub = prompt("Enter third subject marks");
+// var total_marks = prompt("Enter total marks");
+// var obtain_marks = (first_sub) + (second_sub) + (third_sub) ;
+// var per = (first_sub) + (second_sub) + (third_sub) / (total_marks)*100;
+// document.write("Total Marks: " + total_marks + "<br/>");
+// document.write("Marks Obtained: " + obtain_marks + "<br/>");
+// document.write("Percentage: " + per + "<br/>");
+//  if (per >= 80) {
+//    document.write("Grade : A-One" + "<br/>");
+//       document.write("Remarks : Excellent" + "<br/>");
+//  } else if (per >=70 ) {
+//    document.write("Grade : A" + "<br/>");
+//       document.write("Remarks : Good" + "<br/>");
+//  } else if (per >=60 ) {
+//    document.write("Grade : B" + "<br/>");
+//       document.write("Remarks : You need to improve" + "<br/>");
+//  }else if (per < 40 ) {
+//    document.write("Grade : Fail" + "<br/>");
+//       document.write("Remarks : sorry" + "<br/>");
+//  }
+
+// chap 12 - 13
+// if larger number
+// var a = prompt ("Enter any  num");
+// var b = prompt ("Enter any other num");
+// if ( a >= b) {
+// document.write("Equal and Larger Number is: " + a);
+// } else if ( b >= a) {
+//   document.write("Equal and Larger Number is: " + b);
+// }
+
+// show time formate
+// var t = prompt ("Enter time in hour");
+// if ( t >= 0000 && t <1200) {
+//   alert("Good Morning");
+// } else if ( t >=1200 && t < 1700) {
+//   alert("Good Afternoon");
+// } else if ( t >= 1700 && t < 2100) {
+//   alert("Good Evening");
+// }else if ( t >= 2100 && t <= 2359) {
+//   alert("Good Night");
+// }
+
+// check password (Error)
+// var p = "kinzag123";
+// // var x = "enter password";
+// var pas = prompt("enter his/her password");
+// // if( prompt != pas) {
+// //   alert("Enter your Password");
+//   // var pass = prompt("enter his/her password");
+//  if ( pas == p) {
+//   alert("Correct! The password you entered matches the original password.");
+// } else {
+//   alert("Incorrect password”");
+// }
+
+// chap 14- 16
+// var a = prompt("first num");
+// var b = prompt("second num");
+// var c = (a) + (b);
+// alert(c);
+
+// Array literal notation
+// var arr = [];
+// arr[0] = "kinza";
+// alert(arr[0]);
+
+// object notation
+// var arr = new Array();
+// arr = ("kinza g");
+// alert(arr);
+
+// number array
+// var arr = [];
+// arr = [23 , 45 , 70 ];
+// alert(arr[2]);
+
+// store in Array
+// var arr = ["1) SSC" , "2) HSC", "3) BCS" , "4) BS" , "5) BCOM" , "6) MS" , "7) M.Phil."];
+// document.write("Qualification:" + "<br><br><br>");
+// document.write(arr[0] + "<br/>");
+// document.write(arr[1] + "<br/>");
+// document.write(arr[2] + "<br/>");
+// document.write(arr[3] + "<br/>");
+// document.write(arr[4] + "<br/>");
+// document.write(arr[5] + "<br/>");
+// document.write(arr[6] + "<br/>");
+
+// store score percentage in Array
+// var arr = ["Michael" , "John" ,"Tony"];
+// var score = [320, 230 , 480];
+// var total = 500;
+// var per_1 = [320/500*100];
+// var per_2 = [230/500*100];
+// var per_3 = [480/500*100];
+// document.write("Score of Michael is :" + score[0] + "." + "Percentage :" + per_1 + "%" + "<br>");
+// document.write("Score of John is :" + score[1] + "." + "Percentage :" + per_2 +"%" + "<br>");
+// document.write("Score of Tony is :" + score[2] + "." + "Percentage :" + per_3 +"%" + "<br>");
+
+// array using in color
+// var arr  = ["red", "green", "blue"];
+// document.write(arr[0] +"<br>" ,arr[1] + "<br>", arr[2] + "<br>");
+// var arrAsk = prompt("what color he/she wants to add");
+// arr[1] = arrAsk;
+// document.write(arr);
+
+// Array sort
+// var score= [320,230,480,120];
+// document.write("Score of students :" + score + "<br>");
+// document.write("Ordered score of students :" + score.sort() + "<br>");
+
+// show string using array
+// var arr = ["This ", " is ", " my ", " cat"];
+// document.write("Array :" + "<br>" + arr + "<br><br>");
+// document.write("string:" + "<br>" + arr[0] + arr[1] + arr[2] + arr[3]);
